f846cff9b7f4232d9b4e5909bffa4d2203002b64
erdavids <erdavids@ncsu.edu>
2021-06-30 13:12:39 -0400

    Added new smaller image

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import * as React from "react"
  import { StaticImage } from "gatsby-plugin-image"
 
 
-import book from '../images/mockup.png'
+import book from '../images/mockup-smaller.png'
 
 import './style.css'
 
