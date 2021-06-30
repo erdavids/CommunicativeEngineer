aa976b04a4bea927d3b8626a8cd29e04a612261b
erdavids <erdavids@ncsu.edu>
2021-06-30 09:17:06 -0400

    Add initial files

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,183 +1,143 @@
 import * as React from "react"
+import {useState} from "react"
+import {NotificationContainer, NotificationManager} from 'react-notifications';
+ 
+
+import book from '../images/mockup.png'
+import semicolon from '../images/semicolon.png';
+import pr from '../images/PR.png'
+
+import './style.css'
+import 'react-notifications/lib/notifications.css';
 
 // styles
-const pageStyles = {
-  color: "#232129",
-  padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
+
 const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
+  // maxWidth: 320,
+  textAlign: 'center',
 }
-const headingAccentStyles = {
-  color: "#663399",
-}
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-}
-const listItemStyles = {
-  fontWeight: 300,
-  fontSize: 24,
-  maxWidth: 560,
-  marginBottom: 30,
+const fullScreen = {
+  height: '100vh',
+  textAlign: 'center',
 }
 
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: 16,
-  verticalAlign: "5%",
+const emailStyles = {
+  width: "100%",
+  height: "220px",
+  border: "2px #ccc solid !important"
+
 }
 
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  marginBottom: 24,
-}
+const benefits = [
+  {
+    text: "Represent your ideas, beliefs, and desires"
+  },
+  {
+    text: "Build deeper relationships with those around you"
+  },
+  {
+    text: "Develop a less stressful, more fulfilling career"
+  },
+  {
+    text: "Strengthen the overall skills of your team"
+  },
+  {
+    text: "Champion for others and their achievements"
+  }
 
-const descriptionStyle = {
-  color: "#232129",
-  fontSize: 14,
-  marginTop: 10,
-  marginBottom: 0,
-  lineHeight: 1.25,
-}
-
-const docLink = {
-  text: "Documentation",
-  url: "https://www.gatsbyjs.com/docs/",
-  color: "#8954A8",
-}
-
-const badgeStyle = {
-  color: "#fff",
-  backgroundColor: "#088413",
-  border: "1px solid #088413",
-  fontSize: 11,
-  fontWeight: "bold",
-  letterSpacing: 1,
-  borderRadius: 4,
-  padding: "4px 6px",
-  display: "inline-block",
-  position: "relative",
-  top: -2,
-  marginLeft: 10,
-  lineHeight: 1,
-}
-
-// data
-const links = [
-  {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial/",
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-    color: "#E95800",
-  },
-  {
-    text: "How to Guides",
-    url: "https://www.gatsbyjs.com/docs/how-to/",
-    description:
-      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-    color: "#1099A8",
-  },
-  {
-    text: "Reference Guides",
-    url: "https://www.gatsbyjs.com/docs/reference/",
-    description:
-      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-    color: "#BC027F",
-  },
-  {
-    text: "Conceptual Guides",
-    url: "https://www.gatsbyjs.com/docs/conceptual/",
-    description:
-      "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
-    color: "#0D96F2",
-  },
-  {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-    description:
-      "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-    color: "#8EB814",
-  },
-  {
-    text: "Build and Host",
-    url: "https://www.gatsbyjs.com/cloud",
-    badge: true,
-    description:
-      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
-    color: "#663399",
-  },
 ]
 
 // markup
 const IndexPage = () => {
   return (
-    <main style={pageStyles}>
-      <title>Home Page</title>
-      <h1 style={headingStyles}>
-        Congratulations
-        <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! </span>
-        <span role="img" aria-label="Party popper emojis">
-          🎉🎉🎉
-        </span>
-      </h1>
-      <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.js</code> to see this page
-        update in real-time.{" "}
-        <span role="img" aria-label="Sunglasses smiley emoji">
-          😎
-        </span>
-      </p>
-      <ul style={listStyles}>
-        <li style={docLinkStyle}>
-          <a
-            style={linkStyle}
-            href={`${docLink.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-          >
-            {docLink.text}
-          </a>
-        </li>
-        {links.map(link => (
-          <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
-            <span>
-              <a
-                style={linkStyle}
-                href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-              >
-                {link.text}
-              </a>
-              {link.badge && (
-                <span style={badgeStyle} aria-label="New Badge">
-                  NEW!
-                </span>
-              )}
-              <p style={descriptionStyle}>{link.description}</p>
-            </span>
-          </li>
-        ))}
-      </ul>
-      <img
-        alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
-      />
-    </main>
+    <>
+      <div class='pageStyle' style={fullScreen, headingStyles}>
+        <img src={book} width="100%"/>
+        <h1>
+          We have a responsibility to be skilled communicators.
+        </h1>
+      </div>
+      <main class='pageStyle'>
+        <p>
+          A Communicative Engineer is someone that is able to understand and explain.
+        </p>
+        <p>
+          It's someone that is able to take what they know and share it in a personalized way with anyone they're communicating with.
+        </p>
+        <p>
+          This isn't just about technical topics, although that's a big part. Beyond the code and architecture, it's about understanding yourself and others, and being able to use that understanding in a beneficial way.
+        </p>
+        <p>
+          The truth is that most people struggle with communication. It leads to unfortunate misunderstandings, missed opportunities, and unnecessary stress.
+        </p>
+        <p>
+          The goal is to become a Communicative Engineer, and that's what this book is for.
+        </p>
+        <p>
+          It will enable you to do the following...
+        </p>
+
+        <ul>
+          {benefits.map(benefit => (
+            <li>
+              {benefit.text}
+            </li>
+          ))}
+        </ul>
+        <p>
+          The cool part is that all the benefits of purposeful, skilled communication will impact those around you just as strongly.
+        </p>
+        <p>
+          Progress itself will compound as a community is built on a stronger foundation.
+        </p>
+
+        <h2>
+          The Process
+        </h2>
+        <p>
+          Getting better at communication is essentially the same as anything else. It takes a combination of time, intentionality, effort, and discipline.
+        </p>
+        <p>
+          In other words, if you want to get better at something and consistently try to, you will. 
+        </p>
+        <p>
+          This book helps you along the way. 
+        </p>
+        <p>
+          It is intended to be a short guide filled with useful information and suggestions for your journey to becoming a Communicative Engineer.
+        </p>
+        <h2>
+          Writing in Public
+        </h2>
+        <p>
+          In truth, no one is the ultimate expert on this subject, least of all me.
+        </p>
+        <p>
+          I'm working on this book as a way to collect my own thoughts and share them with others, but hopefully with some help.
+        </p>
+        <p>
+          As I finish each stage of the book, I plan to publish as much as I can for direct feedback and editing suggestions. Ultimately I want to draw on the community of people that have built up an immesurable amount of collective experience and make sure I'm producing the best resource possible.
+        </p>
+        <p>
+          If you're interested in being a part of that process, or just want to learn more about the book, consider subscribing below. No spam, I promise.
+        </p>
+
+        <iframe
+          scrolling="no"
+          style={emailStyles}
+          src="https://buttondown.email/thebuffed?as_embed=true"
+          ></iframe><br /><br />
+
+        <div style={headingStyles}>
+          <img
+            
+            alt="Gatsby G Logo"
+            src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
+          />
+        </div>
+      </main>
+      <NotificationContainer/>
+    </>
   )
 }
 
